50b0aaf2fb239b2362668633528211a2d21cf386
zzk <zzkggmy@163.com>
2019-05-18 17:09:57 +0800

    listview

--- lib/discovery.dart
+++ lib/discovery.dart
@@ -22,51 +22,18 @@ class DiscoveryMain extends State<DiscoveryPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final List<WordPair> _suggestions = <WordPair>[];
-    final TextStyle fontBigger = const TextStyle(fontSize: 19.0);
-    final Set<WordPair> _saved = new Set<WordPair>();
 
-    Widget _buildRow(WordPair wordPair) {
-      final bool alreadySaved = _saved.contains(wordPair);
-      return new ListTile(
-        title: new Text(
-          wordPair.asPascalCase,
-          style: fontBigger,
-        ),
-        trailing: new Icon(
-          alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null,
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(wordPair);
-            } else {
-              _saved.add(wordPair);
-            }
-          });
-        },
-      );
-    }
-
-    Widget _buildSuggestions() {
-      return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return new Divider();
-          }
-          final int index = i ~/ 2;
-          if (i >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(i + 1));
-          }
-          return _buildRow(_suggestions[index]);
+    Widget _buildList() {
+      return ListView.builder(
+        itemExtent: 100,
+        itemBuilder: (BuildContext uildContext,int index){
+          return new Text("呱呱",style: TextStyle(color: Colors.green,fontSize: 18),);
         },
       );
     }
 
     return Scaffold(
-      body: _buildSuggestions(),
+      body: _buildList(),
     );
   }
 }
